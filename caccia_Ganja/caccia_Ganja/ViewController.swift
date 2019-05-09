1149433851af6badaf03a8888648d5467f8328c6
leonardo <leonardo.basso@barsanti.edu.it>
2019-05-09 10:55:54 +0200

    inizializzazione file

---
+++ caccia_Ganja/caccia_Ganja/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  caccia_Ganja
+//
+//  Created by Leonardo Basso on 09/05/2019.
+//  Copyright © 2019 Leonardo Basso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
